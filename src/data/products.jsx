f866e5219b94e6f41c489faf9c5ad5ff2b3ee983
gkumar006 <108539198+gkumar006@users.noreply.github.com>
2026-01-13 11:45:30 +0530

    Change iPhone 15 image URL in products data
    Updated the image URL for the iPhone 15 product.

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -1,5 +1,6 @@
 export const products = [
-{ id: 1, name: "iPhone 15", price: 80000, category: "Electronics", rating: 4.5, image: "https://picsum.photos/200/150?1" },
+{ id: 1, name: "iPhone 15", price: 80000, category: "Electronics", rating: 4.5, image: "https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5
+" },
 { id: 2, name: "Nike Shoes", price: 5000, category: "Fashion", rating: 4.2, image: "https://via.placeholder.com/150" },
 { id: 3, name: "Laptop", price: 60000, category: "Electronics", rating: 4.7, image: "https://via.placeholder.com/150" }
 ];
